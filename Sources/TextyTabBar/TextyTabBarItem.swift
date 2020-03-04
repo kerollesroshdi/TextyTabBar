75662696f3fe035cb59323cac75ae16771f320cd
Kerolles Roshdi <kerolles.roshdi@mosandah.com.sa>
2020-03-04 09:33:11 +0200

    Initial Commit

---
+++ Sources/TextyTabBar/TextyTabBarItem.swift
@@ -0,0 +1,14 @@
+//
+//  TextyTabBarItem.swift
+//  TextyTabBarController
+//
+//  Created by Kerolles Roshdi on 2/15/20.
+//  Copyright © 2020 Kerolles Roshdi. All rights reserved.
+//
+
+import UIKit
+
+class TextyTabBarItem: UITabBarItem {
+    @IBInspectable
+    var color: UIColor?
+}
