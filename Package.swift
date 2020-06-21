1aa9590aa9db1fa0d4bc2890796406328d2bca1b
Kerolles Roshdi <kerollesroshdi@users.noreply.github.com>
2020-06-21 16:22:22 +0200

    exclude tests and images

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "TextyTabBar",
+    exclude: ["images", "Tests"],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
